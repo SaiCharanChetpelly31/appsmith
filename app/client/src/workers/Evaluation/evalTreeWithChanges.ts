a78ed823b8c64a66808906accd93485c5ecdbb2d
Vemparala Surya Vamsi <vamsi@appsmith.com>
2024-06-12 13:12:28 +0530

    eval trigger fixes with refactor changes

--- app/client/src/workers/Evaluation/evalTreeWithChanges.ts
+++ app/client/src/workers/Evaluation/evalTreeWithChanges.ts
@@ -1,100 +1,63 @@
-import type { DataTree, UnEvalTree } from "entities/DataTree/dataTreeTypes";
+import type { UnEvalTree } from "entities/DataTree/dataTreeTypes";
 import { dataTreeEvaluator } from "./handlers/evalTree";
-import type { DataTreeDiff } from "@appsmith/workers/Evaluation/evaluationUtils";
 import type { EvalMetaUpdates } from "@appsmith/workers/common/DataTreeEvaluator/types";
-import type { DependencyMap, EvalError } from "utils/DynamicBindingUtils";
 import { makeEntityConfigsAsObjProperties } from "@appsmith/workers/Evaluation/dataTreeUtils";
 import type { EvalTreeResponseData } from "./types";
 import { MessageType, sendMessage } from "utils/MessageUtil";
 import { MAIN_THREAD_ACTION } from "@appsmith/workers/Evaluation/evalWorkerActions";
 import type { UpdateDataTreeMessageData } from "sagas/EvalWorkerActionSagas";
-import type { JSUpdate } from "utils/JSPaneUtils";
 import {
   generateOptimisedUpdatesAndSetPrevState,
   getNewDataTreeUpdates,
   uniqueOrderUpdatePaths,
 } from "./helpers";
+import type DataTreeEvaluator from "workers/common/DataTreeEvaluator";
+import type { JSUpdate } from "utils/JSPaneUtils";
+import type { DataTreeDiff } from "@appsmith/workers/Evaluation/evaluationUtils";
+
+const getDefaultEvalResponse = (): EvalTreeResponseData => ({
+  updates: "[]",
+  dependencies: {},
+  errors: [],
+  evalMetaUpdates: [],
+  evaluationOrder: [],
+  jsUpdates: {},
+  logs: [],
+  unEvalUpdates: [],
+  isCreateFirstTree: false,
+  staleMetaIds: [],
+  removedPaths: [],
+  isNewWidgetAdded: false,
+  undefinedEvalValuesMap: {},
+  jsVarsCreatedEvent: [],
+});
 
 export function evalTreeWithChanges(
   updatedValuePaths: string[][],
   metaUpdates: EvalMetaUpdates = [],
 ) {
-  let evalOrder: string[] = [];
-  let jsUpdates: Record<string, JSUpdate> = {};
-  let unEvalUpdates: DataTreeDiff[] = [];
-  const isCreateFirstTree = false;
-  let dataTree: DataTree = {};
-  const errors: EvalError[] = [];
-  const logs: any[] = [];
-  const dependencies: DependencyMap = {};
-  let evalMetaUpdates: EvalMetaUpdates = [...metaUpdates];
-  let staleMetaIds: string[] = [];
-  const removedPaths: Array<{ entityId: string; fullpath: string }> = [];
-  let unevalTree: UnEvalTree = {};
-
+  let setupUpdateTreeResponse = {} as UpdateTreeResponse;
   if (dataTreeEvaluator) {
-    const setupUpdateTreeResponse =
+    setupUpdateTreeResponse =
       dataTreeEvaluator.setupUpdateTreeWithDifferences(updatedValuePaths);
-
-    evalOrder = setupUpdateTreeResponse.evalOrder;
-    unEvalUpdates = setupUpdateTreeResponse.unEvalUpdates;
-    jsUpdates = setupUpdateTreeResponse.jsUpdates;
-
-    const updateResponse = dataTreeEvaluator.evalAndValidateSubTree(
-      evalOrder,
-      dataTreeEvaluator.oldConfigTree,
-      unEvalUpdates,
-    );
-
-    dataTree = makeEntityConfigsAsObjProperties(dataTreeEvaluator.evalTree, {
-      evalProps: dataTreeEvaluator.evalProps,
-    });
-
-    /** Make sure evalMetaUpdates is sanitized to prevent postMessage failure */
-    evalMetaUpdates = JSON.parse(
-      JSON.stringify([...evalMetaUpdates, ...updateResponse.evalMetaUpdates]),
-    );
-
-    staleMetaIds = updateResponse.staleMetaIds;
-    unevalTree = dataTreeEvaluator.getOldUnevalTree();
   }
-  const allUnevalUpdates = unEvalUpdates.map(
-    (update) => update.payload.propertyPath,
-  );
-  const completeEvalOrder = uniqueOrderUpdatePaths([
-    ...allUnevalUpdates,
-    ...evalOrder,
-  ]);
 
-  const setterAndLocalStorageUpdates = getNewDataTreeUpdates(
-    uniqueOrderUpdatePaths(updatedValuePaths.map((val) => val.join("."))),
-    dataTree,
+  const setterAndLocalStorageUpdatePaths = uniqueOrderUpdatePaths(
+    updatedValuePaths.map((val) => val.join(".")),
   );
 
-  const updates = generateOptimisedUpdatesAndSetPrevState(
-    dataTree,
+  evaluateAndpushUpdatesToMainThread(
     dataTreeEvaluator,
-    completeEvalOrder,
-    setterAndLocalStorageUpdates,
+    setupUpdateTreeResponse,
+    metaUpdates,
+    setterAndLocalStorageUpdatePaths,
   );
+}
 
-  const evalTreeResponse: EvalTreeResponseData = {
-    updates,
-    dependencies,
-    errors,
-    evalMetaUpdates,
-    evaluationOrder: evalOrder,
-    jsUpdates,
-    logs,
-    unEvalUpdates,
-    isCreateFirstTree,
-    staleMetaIds,
-    removedPaths,
-    isNewWidgetAdded: false,
-    undefinedEvalValuesMap: dataTreeEvaluator?.undefinedEvalValuesMap || {},
-    jsVarsCreatedEvent: [],
-  };
-
+const pushResponseToMainThread = (
+  evalTreeResponse: EvalTreeResponseData,
+  unevalTree: UnEvalTree,
+) => {
   const data: UpdateDataTreeMessageData = {
     workerResponse: evalTreeResponse,
     unevalTree,
@@ -107,4 +70,84 @@ export function evalTreeWithChanges(
       method: MAIN_THREAD_ACTION.UPDATE_DATATREE,
     },
   });
+};
+
+const getAffectedNodesInTheDataTree = (
+  unEvalUpdates: DataTreeDiff[],
+  evalOrder: string[],
+) => {
+  const allUnevalUpdates = unEvalUpdates.map(
+    (update) => update.payload.propertyPath,
+  );
+  return uniqueOrderUpdatePaths([...allUnevalUpdates, ...evalOrder]);
+};
+export interface UpdateTreeResponse {
+  unEvalUpdates: DataTreeDiff[];
+  evalOrder: string[];
+  jsUpdates: Record<string, JSUpdate>;
 }
+export const evaluateAndpushUpdatesToMainThread = (
+  dataTreeEvaluator: DataTreeEvaluator | undefined,
+  setupUpdateTreeResponse: UpdateTreeResponse,
+  metaUpdates: EvalMetaUpdates,
+  additionalPathsAddedAsUpdates: string[],
+) => {
+  const defaultResponse = getDefaultEvalResponse();
+
+  if (!dataTreeEvaluator) {
+    const updates = generateOptimisedUpdatesAndSetPrevState(
+      {},
+      dataTreeEvaluator,
+      [],
+    );
+    defaultResponse.updates = updates;
+    defaultResponse.evalMetaUpdates = [...(metaUpdates || [])];
+
+    return pushResponseToMainThread(defaultResponse, {});
+  }
+
+  const { evalOrder, jsUpdates, unEvalUpdates } = setupUpdateTreeResponse;
+  defaultResponse.evaluationOrder = evalOrder;
+  defaultResponse.unEvalUpdates = unEvalUpdates;
+  defaultResponse.jsUpdates = jsUpdates;
+
+  const updateResponse = dataTreeEvaluator.evalAndValidateSubTree(
+    evalOrder,
+    dataTreeEvaluator.oldConfigTree,
+    unEvalUpdates,
+  );
+
+  const dataTree = makeEntityConfigsAsObjProperties(
+    dataTreeEvaluator.evalTree,
+    {
+      evalProps: dataTreeEvaluator.evalProps,
+    },
+  );
+
+  /** Make sure evalMetaUpdates is sanitized to prevent postMessage failure */
+  defaultResponse.evalMetaUpdates = JSON.parse(
+    JSON.stringify([...(metaUpdates || []), ...updateResponse.evalMetaUpdates]),
+  );
+
+  defaultResponse.staleMetaIds = updateResponse.staleMetaIds;
+  const unevalTree = dataTreeEvaluator.getOldUnevalTree();
+
+  const additionalUpdates = getNewDataTreeUpdates(
+    additionalPathsAddedAsUpdates,
+    dataTree,
+  );
+  const affectedNodePaths = getAffectedNodesInTheDataTree(
+    unEvalUpdates,
+    evalOrder,
+  );
+
+  defaultResponse.updates = generateOptimisedUpdatesAndSetPrevState(
+    dataTree,
+    dataTreeEvaluator,
+    affectedNodePaths,
+    additionalUpdates,
+  );
+  dataTreeEvaluator.undefinedEvalValuesMap =
+    dataTreeEvaluator.undefinedEvalValuesMap || {};
+  return pushResponseToMainThread(defaultResponse, unevalTree);
+};
